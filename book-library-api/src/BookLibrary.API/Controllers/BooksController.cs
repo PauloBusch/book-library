4a7fbf137744577ae809f95cb9254a8deb624c7a
Paulo Busch <paulo.busch@codhalum.com.br>
2024-05-07 23:23:17 -0300

    feat: books controller

---
+++ book-library-api/src/BookLibrary.API/Controllers/BooksController.cs
@@ -0,0 +1,23 @@
+﻿using BookLibrary.Domain.Entities;
+using BookLibrary.Domain.Repositories;
+using BookLibrary.Domain.Search;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookLibrary.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class BooksController : ControllerBase
+    {
+        private readonly IBookRepository _bookRepository;
+
+        public BooksController(IBookRepository bookRepository)
+        {
+            _bookRepository = bookRepository;
+        }
+
+        [HttpGet]
+        public Task<IEnumerable<Book>> SearchAsync([FromQuery] BookSearch search)
+            => _bookRepository.GetAllAsync(search);
+    }
+}
