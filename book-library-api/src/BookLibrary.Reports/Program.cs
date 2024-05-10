74bcfb1e998c19e7326c6ade18c35420b66d1dc9
PauloBusch <paulo202015@outlook.com.br>
2024-05-09 23:03:54 -0300

    feat: configure report startup

--- book-library-api/src/BookLibrary.Reports/Program.cs
+++ book-library-api/src/BookLibrary.Reports/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using BookLibrary.Infra;
+
+var configuration = null as IConfiguration;
+var builder = Host.CreateDefaultBuilder(args)
+    .ConfigureAppConfiguration((ctx, configBuilder) =>
+    {
+        configuration = configBuilder
+            .AddJsonFile("appsettings.json", optional: false)
+            .AddJsonFile("appsettings.Development.json", optional: true)
+            .AddEnvironmentVariables()
+        .Build();
+    })
+    .ConfigureServices(services =>
+    {
+        services.AddBookInfraModule(
+            configuration,
+            registerConsumers: configurator => { }
+        );
+    })
+    .ConfigureLogging(logger =>
+    {
+        logger.AddConsole();
+    });
+
+var host = builder.Build();
+await host.RunAsync();
