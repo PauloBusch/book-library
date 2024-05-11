e33004e0d71fad7d77b45c32ad5bd961d9c23d6f
PauloBusch <paulo202015@outlook.com.br>
2024-05-11 01:16:44 -0300

    feat: docker for api

---
+++ mssql-tools-commands.sh
@@ -0,0 +1,5 @@
+echo "Waiting for SQL Server container..."
+sleep 60
+
+echo "Running scripts."
+/opt/mssql-tools/bin/sqlcmd -S sql-server -U sa -P SqlServer2022! -d master -i "/tmp/Scripts/Setup Database.sql"
